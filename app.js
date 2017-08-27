a1f13a513436c211caa5cdb1d7ef04dda01a9c3c
soprajab@hotmail.fr <soprajab@hotmail.fr>
2017-08-27 10:20:49 +0200

    commit

--- app.js
+++ app.js
@@ -1,23 +1,56 @@
-'use strict';
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var index = require('./routes/index');
+var users = require('./routes/users');
 
-var SwaggerExpress = require('swagger-express-mw');
-var app = require('express')();
-module.exports = app; // for testing
+var app = express();
+var mongoose = require('mongoose');
 
-var config = {
-  appRoot: __dirname // required config
-};
+mongoose.connect('mongodb://localhost:27017/usertest');
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
 
-SwaggerExpress.create(config, function(err, swaggerExpress) {
-  if (err) { throw err; }
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-  // install middleware
-  swaggerExpress.register(app);
 
-  var port = process.env.PORT || 10010;
-  app.listen(port);
 
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
+
+router.get('/', function(req, res) {
+  res.json({ message: 'You are running dangerously low on beer!' });
 });
+
+router.use('/', index);
+router.use('/users', users);
+
+// mount the router to the app
+app.use('/api', router);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  console.log('Mon Erreur est ----------', err);
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json('error');
+});
+
+module.exports = app;
